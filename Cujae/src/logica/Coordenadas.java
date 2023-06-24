dd46ad8107e3bddb5aa593dcec650044186c2723
RayM11 <raymaestrex@gmail.com>
2023-06-24 12:17:59 -0400

    Reescribí los cambios perdidos en visitante y coordendas
    visitante actualiza sus posiciones a partir de una lista de direcciones con orden de prioridad
    
    Coordenadas ya es capaz de enviar dicha lista al compararse con otra coordenada

--- Cujae/src/logica/Coordenadas.java
+++ Cujae/src/logica/Coordenadas.java
@@ -1,56 +1,79 @@
 package logica;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+
 import auxiliar.Direccion;
 
 public class Coordenadas {
 
-	private double x;
-	private double y;
+	private float x;
+	private float y;
 
-	public Coordenadas(double x, double y){
+	public Coordenadas(float x, float y){
 		this.x = x;
 		this.y = y;
 	}
 
-	public double getX() {
+	public float getX() {
 		return x;
 	}
-	public double getY() {
+	public float getY() {
 		return y;
 	}
 
-	
-	public Direccion direccionHacia(Coordenadas cor2) throws IllegalArgumentException{
+	/**
+	 *	Recibe una coordenada destino y devuelve una lista de direcciones ordenada según el mayor movimiento 
+	 * 	en cada dirección. Es para tener un orden de prioridad al elegir en qué dirección colocar el movimiento
+	 * 	a la coordenada objetivo
+	 */
+	public ArrayList<Direccion> direccionHacia(Coordenadas cor2){
 		
-		Direccion dir;
+		// Direccion dir;
 		
-		double difY = y - cor2.getY();
-		double difX = x - cor2.getX();
+		float movY = cor2.getY() - y;
+		float movX = cor2.getX() - x; 
 		
-		if (abs(difY) > abs(difX))
+		float movNorte = movY;
+		float movSur = -1*movY;
+		float movEste = movX;
+		float movOeste = -1*movX;
 		
-			if (difY > 0)
-				dir = Direccion.S;
-			else
-				dir = Direccion.N;
-			
-		else if (abs(difY) < abs(difX))
-			if (difX > 0)
-				dir = Direccion.O;
-			else
-				dir = Direccion.E;
-		else
-			throw new IllegalArgumentException("Movimiento diagonal intentado, situaciï¿½n no contemplada todavï¿½a");
-				
-		return dir;
-	}
-	
-	private double abs(double num){
+		final Map<Direccion, Double> desplazamientos = new HashMap<>();
+	    desplazamientos.put(Direccion.N, (double)movNorte);
+	    desplazamientos.put(Direccion.S, (double)movSur);
+	    desplazamientos.put(Direccion.E, (double)movEste);
+	    desplazamientos.put(Direccion.O, (double)movOeste);
 		
-		if (num < 0)
-			num *= -1;
-		
-		return num;
-	}
+	    ArrayList<Direccion> direcciones = new ArrayList<>(desplazamientos.keySet());
+	    Collections.sort(direcciones, new Comparator<Direccion>(){
 
+			@Override
+			public int compare(Direccion d1, Direccion d2) {
+				
+				return Double.compare(desplazamientos.get(d1), desplazamientos.get(d2));
+			}
+	    });
+	    
+	/*	if (abs(movY) > abs(movX))
+		
+			if (movY > 0)
+				dir = Direccion.N;
+			else
+				dir = Direccion.S;
+			
+		else if (abs(movY) < abs(movX))
+			if (movX > 0)
+				dir = Direccion.E;
+			else
+				dir = Direccion.O;
+		else
+			throw new IllegalArgumentException("Movimiento diagonal intentado, situación no contemplada todavía");
+		*/		
+		return direcciones;
+	}
+	
 }
