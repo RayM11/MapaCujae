f0553233cd15bead6cee53485368e25703f2fe5a
RayM11 <raymaestrex@gmail.com>
2023-07-02 01:26:59 -0400

    Squashed commit of the following:
    commit 93b91d21f5ca5506a61ee8874148e0eff82e81c5
    Author: RayM11 <raymaestrex@gmail.com>
    Date:   Sun Jul 2 01:21:02 2023 -0400
    
        Cambios en los evetos del mapa y en las coordenadas
    
    commit f71044e8a9c6de9a05b32f897e552033651fc930
    Merge: 0ed01de eb43f5a
    Author: RayM11 <raymaestrex@gmail.com>
    Date:   Sat Jul 1 15:21:25 2023 -0400
    
        Merge branch 'main' into Me-queme-plp
    
    commit 0ed01de4dbf6f22a0b2403aa574b35526bfd0865
    Author: RayM11 <raymaestrex@gmail.com>
    Date:   Sat Jul 1 13:53:30 2023 -0400
    
        Escalado solucionado
    
        La ventana quedó fea, pero eso se soluciona después. El escalado ya está decente y para que se pudieran ver bien los lugares más adelante cuando estén los marcadores le metí una barra de desplazamiento. La convencional no quería pinchar así q terminé usando un JScrollBar con 2 actionListener plp
    
    commit c9d0022b2e6792046a2e7012e83059bd76bfa928
    Author: ProfeJavix <profejavix@gmail.com>
    Date:   Sat Jul 1 03:32:12 2023 -0500
    
        Habias puesto mal la direccion mijito
    
    commit 30aa1de2347a1d8df897fa0223e548d0bdc036ea
    Author: RayM11 <raymaestrex@gmail.com>
    Date:   Sat Jul 1 03:14:17 2023 -0400
    
        Conquistando Jerusalén II
    
        Habemus mapa
    
    commit 00f6d8f8e335d107c2232e576f015789dc58aba0
    Author: RayM11 <raymaestrex@gmail.com>
    Date:   Fri Jun 30 23:38:01 2023 -0400
    
        La guerra por Jerusalen Comienza

--- Cujae/src/mapaSwing/LabelDeLugarInteresS.java
+++ Cujae/src/mapaSwing/LabelDeLugarInteresS.java
@@ -1,7 +1,18 @@
 package mapaSwing;
 
+import java.awt.Cursor;
 import java.awt.HeadlessException;
+import java.awt.event.MouseListener;
+import java.util.LinkedList;
 
+import javafx.event.EventHandler;
+import javafx.scene.control.Alert;
+import javafx.scene.input.MouseEvent;
+
+import javax.swing.ImageIcon;
+
+import logica.Universidad;
+import mapaFX.MapPanelFX;
 import auxiliar.Convert;
 import cu.edu.cujae.ceis.graph.vertex.Vertex;
 
@@ -12,29 +23,217 @@ public class LabelDeLugarInteresS extends LabelDeLugarS {
 	 */
 	private static final long serialVersionUID = 3412194173818906627L;
 
-	private static String foto = "src/texturas/marcadorLI.png";
-	private static String fotoSelect = "src/texturas/marcadorSelectLI.png";
-	private static int tamano = 6;
+	private static String fotoI = "src/texturas/marcadorLI.png";
+	private static String fotoISelect = "src/texturas/marcadorSelectLI.png";
+	private static int tamano = 30;
+
+	private Vertex vLugar;
 
 
-	public LabelDeLugarInteresS(Vertex vLugar) throws HeadlessException {
-		
-		super(vLugar);
+	public LabelDeLugarInteresS(final Vertex vLugar) throws HeadlessException {
 
-		setIcon(Convert.rezizarImagen(foto, tamano, tamano));				
+		super(vLugar); 
+
+		setIcon(Convert.rezizarImagen(fotoI, tamano, tamano));				
+
+	//	setIcon(new ImageIcon(mapPanelSwing.class.getResource(fotoI)));
+
+		addMouseListener(new MouseListener(){
+
+			@Override
+			public void mouseClicked(java.awt.event.MouseEvent event) {
+
+				// El mapa está en modo de Añadir nuevo punto (este lugar es una posible conexión)
+				if(((mapPanelSwing)getParent()).getModoNuevoPunto()){			
+
+					// Este Lugar está lleno (tiene más de 3 conexiones)
+					if (vLugar.getAdjacents().size() > 3){
+						//Mensaje de error
+						Alert alerta = new Alert(Alert.AlertType.ERROR);
+						alerta.setTitle("Error");
+						alerta.setHeaderText("Intento de cpnexión no válida");
+						alerta.setContentText("Ha intentado conectar el lugar a un punto que ya tiene el máximo de enlaces posibles(4)\nPruebe otro punto");
+						alerta.showAndWait();
+
+						// Este lugar es una conexión válida pero ya estaba seleccionado    
+					}else if (((mapPanelSwing)getParent()).getSeleccion().contains(vLugar)){
+
+						deseleccionar();
+
+						// Este lugar es una conexión válida y no estaba seleccionado	
+					}else if(((mapPanelSwing)getParent()).puedeAgregarSeleccion()){
+
+						seleccionar();
+					}
+					else{
+						Alert alerta = new Alert(Alert.AlertType.ERROR);
+						alerta.setTitle("Error");
+						alerta.setHeaderText("Hay demasiadas conexiones");
+						alerta.setContentText("No puede seleccionar más de 4 lugares a conectar con el nuevo punto\nDesmarque uno de los seleccionados si quiere cambiarlos");
+						alerta.showAndWait();
+					}
+					event.consume();
+
+					// El mapa está en modo Arista
+				}else if (((mapPanelSwing)getParent()).getModoArista()){
+
+					// El Lugar ya está seleccionado
+					if (((mapPanelSwing)getParent()).getSeleccion().contains(vLugar)){
+
+						deseleccionar();
+
+						// El lugar se puede agregar
+					}else if(((mapPanelSwing)getParent()).puedeAgregarSeleccion()){
+
+						// Si tiene que eliminar la arista primero comprueba que existe
+						if (((mapPanelSwing)getParent()).getModoEliminarArista()){
+							if (((mapPanelSwing)getParent()).esAdyacenteA(vLugar))
+								seleccionar();
+							else{
+								Alert alerta = new Alert(Alert.AlertType.ERROR);
+								alerta.setTitle("Error");
+								alerta.setHeaderText("Camino inexistente");
+								alerta.setContentText("Debe seleccionar 2 lugares unidos por un camino para poder eliminar dicho camino");
+								alerta.showAndWait();
+							}
+							// Si tiene que agregar primero comprueba que no existe
+						}else if (!((mapPanelSwing)getParent()).esAdyacenteA(vLugar))		 
+							seleccionar();
+						else{
+							Alert alerta = new Alert(Alert.AlertType.ERROR);
+							alerta.setTitle("Error");
+							alerta.setHeaderText("Camino ya existente");
+							alerta.setContentText("Debe seleccionar 2 Lugares que no estén unidos ya por una camino para poder agregarlo");
+							alerta.showAndWait();
+						}			
+
+						// Ya no se puede agregar otro, así q deselecciona el anterior primero;	
+					}else{
+
+						((mapPanelSwing)getParent()).getLabel(0).deseleccionar();						
+						seleccionar();
+					}
+
+					// El mapa está en Modo Ruta	
+				}else if (((mapPanelSwing)getParent()).getModoRuta()){
+
+					// El lugar ya está seleccionado
+					if (((mapPanelSwing)getParent()).getSeleccion().contains(vLugar)){
+
+						deseleccionar();
+
+						// Si todavía hay otra selección debe haber una ruta, así que actualiza el mapa
+						if (((mapPanelSwing)getParent()).getSeleccion().size() == 1){
+							((mapPanelSwing)getParent()).removerRutaADibujar();
+							getParent().repaint();
+							//-------------	((mapPanelSwing)getParent()).reseleccionarVertices();
+						}
+					
+					// Si no hay ningún otro lugar seleccionado lo selecciona simplemente
+					}else if (((mapPanelSwing)getParent()).getSeleccion().isEmpty()){
+						seleccionar();		
+
+					// Si es el 2do y hay camino desde el primero
+					}else if (((mapPanelSwing)getParent()).hayCaminoA(vLugar)){
+						try {
+							LinkedList<Vertex> ruta = Universidad.getCujae().buscarCaminoMasCorto(((mapPanelSwing)getParent()).getSeleccion().get(0), vLugar);
+							((mapPanelSwing)getParent()).asignarRutaADibujar(ruta);
+							getParent().repaint();
+
+							// Si ya hay un 2do seleccionado lo deselecciona
+							if (((mapPanelSwing)getParent()).getSeleccion().size() == 2)
+								((mapPanelSwing)getParent()).getLabel(1).deseleccionar();
+
+							seleccionar();
+						} catch (Exception e) {
+							e.printStackTrace();
+						}
+
+					// Si no hay un camino hacia este lugar desde el inicial	
+					}else{
+						Alert alerta = new Alert(Alert.AlertType.ERROR);
+						alerta.setTitle("Error");
+						alerta.setHeaderText("No hay camino");
+						alerta.setContentText("No hay forma de conectar ese nodo con el anterior. Esto se debe a una minipulación indebida\n del grafo");
+						alerta.showAndWait();					
+					}
+
+				// Estado normal de seleccion única (eliminación, modificación, selección)
+				}else{
+					if (((mapPanelSwing)getParent()).puedeAgregarSeleccion())
+						seleccionar();
+					else {
+						((mapPanelSwing)getParent()).getLabel(0).deseleccionar();
+						seleccionar();
+					}
+				}
+
+
+				/*			if (((MapPanelFX) getParent()).esperandoNuevoPunto())
+				event.consume();
+
+			else if (((MapPanelFX) getParent()).esperandoNuevaArista())
+				((MapPanelFX) getParent()).devolverVertexAlFormulario(vLugar);
+
+			else{ 
+				if (!((MapPanelFX) getParent()).esperandoDestino())
+					for (Node node : getParent().getChildrenUnmodifiable()){
+						if (node instanceof LabelDeLugarInteres)
+							((LabelDeLugarInteres)node).deseleccionar();
+					}
+				else{
+					try {
+						LinkedList<Vertex> camino = Universidad.getCujae().buscarCaminoMasCorto(((MapPanelFX) getParent()).getVerticeSeleccionado(), vLugar);
+						((MapPanelFX) getParent()).dibujarRuta(camino);
+						((MapPanelFX) getParent()).desactivarSeleccionDestino();
+
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				}
+				seleccionar();
+			}*/
+			}
+
+			@Override
+			public void mouseEntered(java.awt.event.MouseEvent e) {
+				setCursor(new Cursor(Cursor.HAND_CURSOR));
+
+			}
+
+			@Override
+			public void mouseExited(java.awt.event.MouseEvent e) {
+				setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+
+			}
+
+			@Override
+			public void mousePressed(java.awt.event.MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void mouseReleased(java.awt.event.MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+		});	
 
 	}
 
 	public void seleccionar (){
 
-		setIcon(Convert.rezizarImagen(foto, tamano, tamano));
+		setIcon(Convert.rezizarImagen(fotoISelect, tamano, tamano));
 		((mapPanelSwing)getParent()).agregarSeleccion(vLugar);
+		getParent().repaint();
 		// Llamar al llenado de la info
 	}
 	public void deseleccionar(){
 
-		setIcon(Convert.rezizarImagen(fotoSelect, tamano, tamano));
+		setIcon(Convert.rezizarImagen(fotoI, tamano, tamano));
 		((mapPanelSwing)getParent()).removerSeleccion(vLugar);
+		getParent().repaint();
 		//limpiar el panel de la info
 	}
 
