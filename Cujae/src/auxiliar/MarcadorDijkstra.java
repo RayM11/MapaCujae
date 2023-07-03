5b9d133b6e0475cb074e6eddf12ab2947633be8b
ProfeJavix <profejavix@gmail.com>
2023-07-02 22:36:11 -0500

    Dijkstra pincha

--- Cujae/src/auxiliar/MarcadorDijkstra.java
+++ Cujae/src/auxiliar/MarcadorDijkstra.java
@@ -1,26 +1,21 @@
 package auxiliar;
 
-import cu.edu.cujae.ceis.graph.edge.Edge;
 import cu.edu.cujae.ceis.graph.vertex.Vertex;
 
 public class MarcadorDijkstra implements Comparable<MarcadorDijkstra>{
 	
 	private Vertex verticeAnterior;
-	private Edge menorArco;
 	private double distancia;
 	
 	public Vertex getVerticeAnterior() {
 		return verticeAnterior;
 	}
-	public Edge getMenorArco() {
-		return menorArco;
-	}
+	
 	public double getDistancia() {
 		return distancia;
 	}
-	public MarcadorDijkstra(Vertex verticeAnterior, Edge menorArco, double distancia) {
+	public MarcadorDijkstra(Vertex verticeAnterior, double distancia) {
 		this.verticeAnterior = verticeAnterior;
-		this.menorArco = menorArco;
 		this.distancia = distancia;
 	}
 	
